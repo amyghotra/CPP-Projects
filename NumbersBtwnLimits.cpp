5eff8d5ec58f0793bb3e3c737e3849e708412fc9
Amy Ghotra <amy.ghotra8@gmail.com>
2019-04-29 20:49:01 -0400

    Update and rename LAB2B.cpp to NumbersBtwnLimits.cpp

--- NumbersBtwnLimits.cpp
+++ NumbersBtwnLimits.cpp
@@ -1,10 +1,8 @@
 /*
  Author: amy ghotra
- Course: CSCI-136
- Instructor: maryash
- Assignment: lab2b
  
- This program asks the user for two numbers, which then become the upper and lower limits. The program then goes on to out put all numbers between and including the limits
+ This program asks the user for two numbers, which then become the upper and lower limits. 
+ The program then goes on to output all numbers between and including the limits
  */
 
 #include <iostream>
@@ -12,15 +10,18 @@ using namespace std;
 
 int main()
 {
+    // variables to store the limits
     int first_input;
     int second_input;
     int U;
     int L;
     int i;
+    // get limits from user
     cout << "Enter a number ";
     cin >> first_input;
     cout << "Enter another numnber ";
     cin >> second_input;
+    // ensure that the first limit is the upper limit and the second input is the lower limit
     if (first_input > second_input) {
         U = first_input;
         L = second_input;
@@ -29,6 +30,7 @@ int main()
         U = second_input;
         L = first_input;
     }
+    // use for loop to print out the numbers
     for (int i = L; i < U; i++) {
         cout << i << endl;
     }
